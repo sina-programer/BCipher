b20978fe4bc90db19a4d1696bafdc158df1c5712
Sina <sina.programerr@gmail.com>
2024-02-20 00:44:58 +0330

    some changes

--- main.py
+++ main.py
@@ -51,27 +51,35 @@ if __name__ == "__main__":
     parser = ArgumentParser(prog='BCipher', description=_description)
     parser.add_argument('source', help='desired pdf book')
     parser.add_argument('data', help='either a text between " or path of a text file')
-    parser.add_argument('-e', '--encrypt', action='store_true', help='do encryption')
-    parser.add_argument('-d', '--decrypt', action='store_true', help='do decryption')
+    parser.add_argument('-e', '--encrypt', action='store_true', help='do encrypt operation')
+    parser.add_argument('-d', '--decrypt', action='store_true', help='do decrypt operation')
     parser.add_argument('-o', '--output', help='export the result in this path')
 
     args = parser.parse_args()
     if os.path.exists(args.data):
+        print(f"loading the file you entered as data: <{args.data}>")
         with open(args.data) as handler:
             args.data = handler.read()
+
+    if not os.path.exists(args.source):
+        parser.error(f"the entered source is not valid: <{args.source}>")
 
     book = pdf.PdfFileReader(args.source)
     if is_pdf_valid(book):
         content = list(map(extractor, book.pages))
         if args.decrypt:
             result = decrypt(args.data, content)
+        elif args.encrypt:
+            result = encrypt(args.data, content)
         else:
+            print("you didn't set the operation, encryption is running by default")
             result = encrypt(args.data, content)
         print("Result:", result)
 
         if args.output:
             with open(args.output, 'w') as handler:
                 handler.write(result)
+            print(f"the result is written in output: <{args.output}>")
 
     else:
         print('Oops, the program could not detect any text in the loaded pdf')
