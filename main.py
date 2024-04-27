b66e1456135d2ad4715d2255131a26d0e8610fe7
Sina.F <sina.programerr@gmail.com>
2024-04-27 12:24:23 +0330

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ import time
 import os
 
 def encrypt_word(word, contents: list[str]):
-    if coords := check_word(word, contents):
-        return '-'.join(coords)
+    if coords := fetch_word_pos(word, contents):
+        return '-'.join(map(str, coords))
     return '#'
 
 def encrypt(text, contents: list[str], delimiter=','):
+    encrypter = partial(encrypt_word, contents=contents)
     return '\n'.join(
         [
             delimiter.join(
                 map(
-                    partial(encrypt_word, contents=contents),
+                    encrypter,
                     line
                 )
             )
@@ -39,7 +40,7 @@ def decrypt(phrase, contents: list[str], delimiter=','):
             words.append('#')
     return ' '.join(words)
 
-def check_word(word, contents):
+def fetch_word_pos(word, contents):
     for page_idx, page_text in enumerate(contents):
         if word in page_text:
             for line_idx, line in enumerate(page_text.splitlines()):
@@ -52,7 +53,7 @@ def check(clause, contents):
     for line in splitter(clause):
         _result = []
         for word in line:
-            if check_word(word, contents):
+            if fetch_word_pos(word, contents):
                 _result.append(word)
             else:
                 _result.append('#')
@@ -103,7 +104,7 @@ FIGLET = '''\n
 if __name__ == "__main__":
     parser = ArgumentParser(prog='BCipher', description=DESCRIPTION)
     parser.add_argument('source', help='desired pdf book')
-    parser.add_argument('data', help='either a text between " or path of a text file')
+    parser.add_argument('data', help='either a text between " or path of a text file. words separated by space')
     parser.add_argument('-c', '--check', action='store_true', help='do checking operation | check the presence of <data> values & put # for unaccessables')
     parser.add_argument('-e', '--encrypt', action='store_true', help='do encrypt operation | convert each word into a 3-part encoded clause')
     parser.add_argument('-d', '--decrypt', action='store_true', help='do decrypt operation | retrieve each encoded clause into a word')
