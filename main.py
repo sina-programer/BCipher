c917bc3aa141cfd58da7104ea9cf8a13c1aa1837
Sina.F <sina.programerr@gmail.com>
2024-04-30 18:40:33 +0330

    remove 'check' operation

--- main.py
+++ main.py
@@ -56,17 +56,6 @@ def decrypt(phrase, contents: list[str], delimiter='.'):
     decrypter = partial(decrypt_code, contents=contents)
     return ' '.join(map(decrypter, phrase.split(delimiter)))
 
-def check(clause, contents):
-    result = []
-    for line in splitter(clause):
-        _result = []
-        for word in line:
-            if fetch_word_pos(word, contents):
-                _result.append(word)
-            else:
-                _result.append('#')
-        result.append(' '.join(_result))
-    return '\n'.join(result)
 
 def is_pdf_valid(pdf_obj):
     """ Has the program detected any text in pdf or not """
@@ -104,7 +93,6 @@ if __name__ == "__main__":
     parser = ArgumentParser(prog='BCipher', description=DESCRIPTION)
     parser.add_argument('source', help='desired pdf book')
     parser.add_argument('data', help='either a text between " or path of a text file. words separated by space')
-    parser.add_argument('-c', '--check', action='store_true', help='do checking operation | check the presence of <data> values & put # for unaccessables')
     parser.add_argument('-e', '--encrypt', action='store_true', help='do encrypt operation | convert each word into a 3-part encoded clause')
     parser.add_argument('-d', '--decrypt', action='store_true', help='do decrypt operation | retrieve each encoded clause into a word')
     parser.add_argument('-o', '--output', help='export the result in this path')
@@ -130,8 +118,6 @@ if __name__ == "__main__":
         if args.lower_data:
             args.data = args.data.lower()
 
-        if args.check:
-            result = check(args.data, content)
         if args.decrypt:
             result = decrypt(args.data, content, delimiter=args.delimiter)
         elif args.encrypt:
